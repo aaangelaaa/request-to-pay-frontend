703b3077d10b3bae6bbeaf456dc9c22ba737db71
Angela <aaangelaaa@users.noreply.github.com>
2019-10-31 12:10:47 -0400

    Added react bootstrap to package.

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -5,6 +5,8 @@ import BackendAuth from '../../helpers/BackendAuth.js'
 import LoginConfirmation from '../login confirmation/LoginConfirmation.js'
 import './LoginPage.css';
 
+{/* Login Page */}
+
 class LoginPage extends Component {
     constructor(props) {
         super(props);
@@ -25,6 +27,7 @@ class LoginPage extends Component {
         this.username = e.target.value;
         console.log(this.username);
     }
+
     handlePassword (e) {
         e.preventDefault();
         this.setState({ password: e.target.value });
@@ -43,7 +46,6 @@ class LoginPage extends Component {
         return (
             <div className="login">
                 <h3 className="login-header">Login</h3>
-                //<div className="container">
                     <Form onSubmit={this.handleClick}>
                         <Form.Label>Username</Form.Label>
                         <Form.Control required type="text" placeholder="Username" 
@@ -63,7 +65,6 @@ class LoginPage extends Component {
                             </Button>
                         </div>
                     </Form>
-                //</div>
             </div>
         );
     }
